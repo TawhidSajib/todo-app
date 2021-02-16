ec60df7171d32200df5edf263cf069b95899a6e9
TawhidSajib <tauhidulsajib34@gmail.com>
2021-02-16 19:03:39 +0600

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -15,10 +15,13 @@
         </v-col>
          <v-btn color="success" v-if="!isEditing" @click="submit">Add</v-btn>
          <v-btn color="primary" v-else @click="update">Update</v-btn>
+         
+
     </v-row> 
     <ol>
     <li v-for="(todo,index) in todos" :key="index">
-      {{ todo }}
+       <input type="checkbox" v-on:change="completeTask(todo)" v-bind:checked="todo.isComplete"/>
+       <span v-bind:class="{completed: todo.isComplete}">{{todo.title}}</span>
       <v-btn color="primary" style="margin-right: 3px;margin-left: 3px;" @click="edit(todo,index)">Edit</v-btn>
       <v-btn color="error" style="margin-left: 3px;" @click="remove(index)">Delete</v-btn>
     </li>  
@@ -30,15 +33,18 @@
 export default {
   data(){
     return {
+      
       isEditing: false,
       todo: '',
-      todos: [],
-      selectId: ''
+      todos: [{title: 'Go home',isComplete: false},{title: 'Pack bag',isComplete: false},{title: 'Go for hair cut',isComplete: false}],
+      selectId: '',
+      completed: true
     }
   },
+ 
   methods: {
        submit(){
-         this.todos.push(this.todo)
+         this.todos.push({title: this.todo,isComplete: false})
          this.todo = ''
        },
        remove(id){
@@ -46,7 +52,7 @@ export default {
        },
        edit(data,id){
          this.isEditing = true
-         this.todo = data
+         this.todo = data.title
          this.selectId = id
 
        },
@@ -54,7 +60,17 @@ export default {
          this.isEditing = false
          this.todos.splice(this.selectId,1,this.todo)
          this.todo = ''
-       }
+       },
+       completeTask(todo){
+         todo.isComplete = !todo.isComplete
+       },
+     
+       
   }
 }
 </script>
+<style scoped>
+.completed{
+  text-decoration: line-through;
+}
+</style>
