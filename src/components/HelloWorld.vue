85fd7bce91c1fcfc1d598ae09d9fa97bb4015292
TawhidSajib <tauhidulsajib34@gmail.com>
2021-02-16 15:10:07 +0600

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,60 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+<v-container>
+    <v-row>
+        <v-col
+          cols="12"
+          sm="6"
+          md="4"
+        >
+          <v-text-field
+            dense
+            label="Tod0-list"
+            v-model="todo"
+          ></v-text-field>
+          
+        </v-col>
+         <v-btn color="success" v-if="!isEditing" @click="submit">Add</v-btn>
+         <v-btn color="primary" v-else @click="update">Update</v-btn>
+    </v-row> 
+    <ol>
+    <li v-for="(todo,index) in todos" :key="index">
+      {{ todo }}
+      <v-btn color="primary" style="margin-right: 3px;margin-left: 3px;" @click="edit(todo,index)">Edit</v-btn>
+      <v-btn color="error" style="margin-left: 3px;" @click="remove(index)">Delete</v-btn>
+    </li>  
+    </ol>             
+</v-container>
+  
 </template>
-
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data(){
+    return {
+      isEditing: false,
+      todo: '',
+      todos: [],
+      selectId: ''
+    }
+  },
+  methods: {
+       submit(){
+         this.todos.push(this.todo)
+         this.todo = ''
+       },
+       remove(id){
+         this.todos.splice(id,1)
+       },
+       edit(data,id){
+         this.isEditing = true
+         this.todo = data
+         this.selectId = id
+
+       },
+       update(){
+         this.isEditing = false
+         this.todos.splice(this.selectId,1,this.todo)
+         this.todo = ''
+       }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
